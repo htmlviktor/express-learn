f1f5984920626a29f5c60063426b0c2b7b70a9ac
viktor kan <viktorkan9@gmail.com>
2020-04-03 13:29:12 +0300

    Закончил курс по EXPRESS

--- routes/card.js
+++ routes/card.js
@@ -12,10 +12,17 @@ router.post('/add', async (req, res) => {
 router.get('/', async (req, res) => {
     const card = await Card.fetch();
     res.render('card', {
-        title: 'Корзина',
+        title: 'Card',
         courses: card.courses,
         price: card.price
     })
 });
+
+router.delete('/remove/:id', async (req, res) => {
+    const card = await Card.remove(req.params.id);
+    res.status(200).json(card);
+});
+
+
 
 module.exports = router;
