7c5a2dd8ce60b8d713f3892c7221a29686a39687
viktor kan <viktorkan9@gmail.com>
2020-04-09 11:20:20 +0300

    Удаление из корзины Урок - 10

--- routes/card.js
+++ routes/card.js
@@ -28,8 +28,15 @@ router.get('/', async (req, res) => {
 });
 
 router.delete('/remove/:id', async (req, res) => {
-    const card = await Card.remove(req.params.id);
-    res.status(200).json(card);
+    await req.user.removeFromCart(req.params.id);
+    const user = await req.user.populate('cart.items.courseId').execPopulate();
+
+    const courses = mapCartItems(user.cart);
+    const cart = {
+        courses, price: 0
+    }
+
+    res.status(200).json(cart);
 });
 
 
